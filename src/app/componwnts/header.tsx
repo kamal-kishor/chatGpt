ebc06eb0b0f1431febd33f6151e9f10ed1515d25
Kamal-Joshi <kkjoshi.kk@gmail.com>
2023-11-30 13:26:27 +0530

    initial commit

---
+++ src/app/componwnts/header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import { Alert, Button, Select } from "iron-components-ui";
+
+const Header = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
+      {/* <h2>header</h2>
+      <Alert />
+      <Select option={["orange", "apple", "mango"]} />
+      <Button /> */}
+      <h2 className="text-5xl font-bold mb-20">ChatGPT</h2>
+    </div>
+  );
+};
+
+export default Header;
